da8363a0e6a5bd4b0a138f2f64b07b01dac9534c
EloyNunayalle <llorent.nunayalle@utec.edu.pe>
2025-07-09 04:47:25 -0500

    version final al 98, eliminó archivos antiguos

--- AED-kdtree2d/js/AnimationMain.js
+++
@@ -1,6 +0,0 @@
-import { draw } from './ObjectManager.js';
-
-const canvas=document.getElementById('board');
-const ctx=canvas.getContext('2d');
-
-export function render(){ draw(ctx); }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # proyecto-aed
+
+Arbol kd
+
+
